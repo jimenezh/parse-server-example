cc31a9706a8cd2c48efa7337f51a04a5a3cf72fb
jimenezh <jimenezshr@fb.com>
2020-07-29 15:09:28 -0700

    Commented out

--- cloud/main.js
+++ cloud/main.js
@@ -40,24 +40,25 @@ Parse.Cloud.define('pushChannelTest', function(request, response) {
      console.log("#### PUSH ERROR" + error.message);
   }, useMasterKey: true});
 
-  response.success('success');
+
+  // response.success('success');
 });
 
 // iOS push testing
 Parse.Cloud.define("iosPushTest", function(request, response) {
 
-  // request has 2 parameters: params passed by the client and the authorized user                                                                                                                               
+  // request has 2 parameters: params passed by the client and the authorized user
   var params = request.params;
   var user = request.user;
 
-  // Our "Message" class has a "text" key with the body of the message itself                                                                                                                                    
+  // Our "Message" class has a "text" key with the body of the message itself
   var messageText = params.text;
 
   var pushQuery = new Parse.Query(Parse.Installation);
-  pushQuery.equalTo('deviceType', 'android'); // targeting android devices only                                                                                                                                          
+  pushQuery.equalTo('deviceType', 'android'); // targeting android devices only
 
   Parse.Push.send({
-    where: pushQuery, // Set our Installation query                                                                                                                                                              
+    where: pushQuery, // Set our Installation query
     data: {
       alert: "Message: " + messageText
     }
@@ -69,4 +70,3 @@ Parse.Cloud.define("iosPushTest", function(request, response) {
 
   response.success('success');
 });
-
